5f94137565e63420fdd727f0a0f1393823c92f7b
johnsey <johnsey.reynolds@gmail.com>
2022-05-03 10:51:19 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // const task = document.getElementById("new-task-description")
+
+  const form = document.querySelector("form")
+  const tasks = document.querySelector("#tasks")
+  let button = document.createElement("btn")
+
+  button.addEventListener("click",e=>deleteButton(e))
+
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputValue = e.target.task.value
+    addElement(inputValue)
+  })
+  function addElement(element) {
+    let p = document.createElement("p")
+    p.textContent = ` ${element} `
+    tasks.appendChild(p)
+  }
+  function deleteButton(event){
+
+  }
 });
